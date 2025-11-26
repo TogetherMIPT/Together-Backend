83d097119c014a246dfaf264495425e1a7dcb28e
Vagapov Gleb <vagapovge@student.bmstu.ru>
2025-11-26 19:07:47 +0300

    commented examples

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,6 @@ package main
 import (
 	"log"
 	"myapp/database"
-	"myapp/models"
-	"myapp/utils"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 func main() {
@@ -20,9 +15,6 @@ func main() {
 	}
 	defer database.Close()
 
-	// Автомиграция???
-	//db.AutoMigrate(&models.User{}, &models.LinkToken{}, &models.Chat{}, &models.Message{}, &models.Relation{})
-
 	// Выполняем миграции
 	if err := database.Migrate(database.DB); err != nil {
 		log.Fatal("Failed to migrate database:", err)
@@ -31,75 +23,75 @@ func main() {
 	log.Println("Application started successfully!")
 
 	// Пример создания пользователя с хешированным паролем
-	password := "SecurePass123!"
-	hashedPassword, err := utils.HashPassword(password)
-	if err != nil {
-		log.Printf("Error hashing password: %v", err)
-		return
-	}
+	// password := "SecurePass123!"
+	// hashedPassword, err := utils.HashPassword(password)
+	// if err != nil {
+	// 	log.Printf("Error hashing password: %v", err)
+	// 	return
+	// }
 
-	user := &models.User{
-		Name:      "Иван Иванов",
-		Email:     "ivan@example.com",
-		Login:     "ivan123",
-		Country:   "Russia",
-		City:      "Moscow",
-		Birthdate: time.Date(1990, 5, 15, 0, 0, 0, 0, time.UTC),
-		Gender:    "male",
-		Password:  hashedPassword,
-	}
+	// user := &models.User{
+	// 	Name:      "Иван Иванов",
+	// 	Email:     "ivan@example.com",
+	// 	Login:     "ivan123",
+	// 	Country:   "Russia",
+	// 	City:      "Moscow",
+	// 	Birthdate: time.Date(1990, 5, 15, 0, 0, 0, 0, time.UTC),
+	// 	Gender:    "male",
+	// 	Password:  hashedPassword,
+	// }
 
-	// Создаем пользователя в БД
-	if err := database.DB.Create(user).Error; err != nil {
-		log.Printf("Error creating user: %v", err)
-	} else {
-		log.Printf("User created with ID: %d", user.UserID)
-	}
+	// // Создаем пользователя в БД
+	// if err := database.DB.Create(user).Error; err != nil {
+	// 	log.Printf("Error creating user: %v", err)
+	// } else {
+	// 	log.Printf("User created with ID: %d", user.UserID)
+	// }
 
 	// Пример создания токена для пользователя
-	token := &models.LinkToken{
-		Token:              uuid.New().String(),
-		UserID:             user.UserID,
-		ExpirationDatetime: time.Now().Add(24 * time.Hour), // Токен действителен 24 часа
-	}
+	// token := &models.LinkToken{
+	// 	Token:              uuid.New().String(),
+	// 	UserID:             user.UserID,
+	// 	ExpirationDatetime: time.Now().Add(24 * time.Hour), // Токен действителен 24 часа
+	// }
 
-	if err := database.DB.Create(token).Error; err != nil {
-		log.Printf("Error creating token: %v", err)
-	} else {
-		log.Printf("Token created: %s", token.Token)
-	}
+	// if err := database.DB.Create(token).Error; err != nil {
+	// 	log.Printf("Error creating token: %v", err)
+	// } else {
+	// 	log.Printf("Token created: %s", token.Token)
+	// }
 
 	// Пример создания чата
-	chat := &models.Chat{
-		UserID:   user.UserID,
-		ChatName: "Мой первый чат",
-		IsActive: true,
-	}
+	// chat := &models.Chat{
+	// 	UserID:   user.UserID,
+	// 	ChatName: "Мой первый чат",
+	// 	IsActive: true,
+	// }
 
-	if err := database.DB.Create(chat).Error; err != nil {
-		log.Printf("Error creating chat: %v", err)
-	} else {
-		log.Printf("Chat created with ID: %d", chat.ChatID)
-	}
+	// if err := database.DB.Create(chat).Error; err != nil {
+	// 	log.Printf("Error creating chat: %v", err)
+	// } else {
+	// 	log.Printf("Chat created with ID: %d", chat.ChatID)
+	// }
 
 	// Пример создания сообщения
-	message := &models.Message{
-		ChatID:      chat.ChatID,
-		MessageText: "Привет, это первое сообщение!",
-	}
+	// message := &models.Message{
+	// 	ChatID:      chat.ChatID,
+	// 	MessageText: "Привет, это первое сообщение!",
+	// }
 
-	if err := database.DB.Create(message).Error; err != nil {
-		log.Printf("Error creating message: %v", err)
-	} else {
-		log.Printf("Message created with ID: %d", message.MessageID)
-	}
+	// if err := database.DB.Create(message).Error; err != nil {
+	// 	log.Printf("Error creating message: %v", err)
+	// } else {
+	// 	log.Printf("Message created with ID: %d", message.MessageID)
+	// }
 
 	// Пример поиска пользователя с загрузкой связанных данных
-	var foundUser models.User
-	if err := database.DB.Preload("Chats").Preload("LinkTokens").First(&foundUser, user.UserID).Error; err != nil {
-		log.Printf("Error finding user: %v", err)
-	} else {
-		log.Printf("Found user: %s with %d chats and %d tokens",
-			foundUser.Name, len(foundUser.Chats), len(foundUser.LinkTokens))
-	}
+	// var foundUser models.User
+	// if err := database.DB.Preload("Chats").Preload("LinkTokens").First(&foundUser, user.UserID).Error; err != nil {
+	// 	log.Printf("Error finding user: %v", err)
+	// } else {
+	// 	log.Printf("Found user: %s with %d chats and %d tokens",
+	// 		foundUser.Name, len(foundUser.Chats), len(foundUser.LinkTokens))
+	// }
 }
